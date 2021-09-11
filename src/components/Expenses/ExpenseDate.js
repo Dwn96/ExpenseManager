48da8dc6e543555b8d9db2ba722650c3737cc1f4
Dan Njeru <wambu43@gmail.com>
2021-09-11 14:42:52 +0300

    setup increment date on click

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseDate.css'
 
 function ExpenseDate({date}) {
     const month = date.toLocaleString('en-US',{month:'long'})
-    const day =  date.toLocaleString('en-US',{day:'2-digit'})
+    //const day =  date.toLocaleString('en-US',{day:'2-digit'})
     const year = date.getFullYear()
 
+    const [day, setDay] = useState(date.toLocaleString('en-US',{day:'2-digit'}))
+
+    const increamentDay = () => {
+        setDay(parseInt(day) + 1)
+    }
+   
     return (
         
 
-        <div className = "expense-date">
+        <div className = "expense-date" >
 
         <div className="expense-date__month">{month}</div>
         <div  className="expense-date__year">{year}</div>
-        <div  className="expense-date__day">{day}</div>
+        <div  className="expense-date__day" onClick = {increamentDay}>{day}</div>
         
       
         </div>
